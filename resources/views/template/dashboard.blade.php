1e37fd5662e73bf2fa37406ab5af53cfccb354a8
Thesarr30 <thesarkun@gmail.com>
2024-05-15 16:06:34 +0700

    done banh

---
+++ resources/views/template/dashboard.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Star Admin2 </title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="{{ asset('star-admin2-free-admin-template-main/src/assets/vendors/feather/feather.css') }}">
+  <link rel="stylesheet" href="{{ asset('star-admin2-free-admin-template-main/src/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('star-admin2-free-admin-template-main/src/assets/vendors/ti-icons/css/themify-icons.css') }}">
+  <link rel="stylesheet" href="{{ asset('star-admin2-free-admin-template-main/src/assets/vendors/typicons/typicons.css') }}">
+  <link rel="stylesheet" href="{{ asset('star-admin2-free-admin-template-main/src/assets/vendors/simple-line-icons/css/simple-line-icons.css') }}">
+  <link rel="stylesheet" href="{{ asset('star-admin2-free-admin-template-main/src/assets/vendors/css/vendor.bundle.base.css') }}">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{ asset('star-admin2-free-admin-template-main/src/assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('star-admin2-free-admin-template-main/src/assets/js/select.dataTables.min.css') }}">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{ asset('star-admin2-free-admin-template-main/src/assets/css/vertical-layout-light/style.css') }}">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="{{ asset('star-admin2-free-admin-template-main/src/assets/images/favicon.png') }}" />
+</head>
+
+
+      <!-- partial -->
+      @include('template.components.header')
+      @include('template.components.settings')
+      @include('template.components.footer')
+
+      
+<!-- plugins:js -->
+  <script src="{{ asset('star-admin2-free-admin-template-main/src/assets/vendors/js/vendor.bundle.base.js') }}"></script>
+  <script src="{{ asset('star-admin2-free-admin-template-main/src/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="{{ asset('star-admin2-free-admin-template-main/src/assets/vendors/chart.js/Chart.min.js') }}"></script>
+  <script src="{{ asset('star-admin2-free-admin-template-main/src/assets/vendors/progressbar.js/progressbar.min.js') }}"></script>
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="{{ asset('star-admin2-free-admin-template-main/src/assets/js/off-canvas.js') }}"></script>
+  <script src="{[asset('star-admin2-free-admin-template-main/src/assets/js/hoverable-collapse.js')]}"></script>
+  <script src="{{ asset('star-admin2-free-admin-template-main/src/assets/js/template.js') }}"></script>
+  <script src="{{ asset('star-admin2-free-admin-template-main/src/assets/js/settings.js') }}"></script>
+  <script src="{{ asset('star-admin2-free-admin-template-main/src/assets/js/todolist.js') }}"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="{{ asset('star-admin2-free-admin-template-main/src/assets/js/jquery.cookie.js') }}" type="text/javascript"></script>
+  <script src="{{ asset('star-admin2-free-admin-template-main/src/assets/js/dashboard.js') }}"></script>
+  <script src="{{ asset('star-admin2-free-admin-template-main/src/assets/js/proBanner.js') }}"></script>
+  <!-- <script src="../../assets/js/Chart.roundedBarCharts.js"></script> -->
+  <!-- End custom js for this page-->
